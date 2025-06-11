69c27affa27c967a76b4ba4b4166e6b9eabfc4ab
Erfan <erfan.norozi54@gmail.com>
2025-06-11 22:34:58 +0330

    refactor: replace inline Google Tag Manager script with Next.js Script component for improved performance and maintainability

--- front_end/src/app/layout.tsx
+++ front_end/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import ClientLayout from "../components/layout/ClientLayout";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -77,16 +78,15 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl" className="scroll-smooth">
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                `,
-          }}
-        />
+      <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KRPC7WM6');
+          `}
+        </Script>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=5"
