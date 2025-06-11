85f8b3bb5beb0aa1421b7c4b31b6517b322e13d5
Erfan <erfan.norozi54@gmail.com>
2025-06-11 20:39:17 +0330

    feat: add Google Tag Manager script to RootLayout for enhanced tracking

--- front_end/src/app/layout.tsx
+++ front_end/src/app/layout.tsx
@@ -77,6 +77,16 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl" className="scroll-smooth">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                `,
+          }}
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=5"
